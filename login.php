f44819971e1e4f44ca76ae15b77e1c0114e7874a
김윤엽 <dbsduq9655@naver.com>
2018-01-03 16:43:16 +0900

    로그인 처리 (미완)

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+//DB connect
+$host = '127.0.0.1';
+$user = 'root';
+$pw = '!As092212';
+$dbName = 'managetic';
+$mysqli = new mysqli($host, $user, $pw, $dbName);
+
+//login parameter
+$id = $_POST['id'];
+$pw = $_POST['pw'];
+?>
