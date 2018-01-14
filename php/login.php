48c9420aaf5a29b02ee344579d658f1ae386ef5f
김윤엽 <dbsduq9655@naver.com>
2018-01-14 20:43:03 +0900

    login.php

--- php/login.php
+++ php/login.php
@@ -1,15 +1,28 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+<body>
 <?php
+	session_start();
+	
+	include("dbcon.php");	
+	
+	$id = $_POST['id'];
+	$pw = $_POST['pw'];
 
-session start();
-
-//DB connect
-$host = '127.0.0.1';
-$user = 'root';
-$pw = '!As092212';
-$dbName = 'managetic';
-$mysqli = new mysqli($host, $user, $pw, $dbName);
-
-//login parameter
-$id = $_POST['id'];
-$pw = $_POST['pw'];
-?>
+	$sql = "SELECT * FROM user WHERE id= '$id' AND pw= '$pw'";
+	$res = $mysqli->query($sql); 
+	$row = $res->fetch_array(MYSQLI_ASSOC);
+	
+	if($row != null){
+		$_SESSION['user_name'] = $row['name'];
+		echo ("<script>alert('".$row['name']."님 어서오세요.');</script>");
+		echo("<script>location.href='../mypage.html';</script>");
+	}
+	if($row == null){
+		echo "<script>alert('아이디 혹은 비밀번호가 옳바르지 않습니다.');</script>";	
+		echo("<script>location.href='../index.html';</script>");
+	}
+	?>
+</body>
